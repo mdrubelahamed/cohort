9397cf81c7b93cd977111b2bbcd4234e6424f8eb
Rubel <work.mdrubelahamed@gmail.com>
2024-08-22 08:39:11 +0530

    Change shell modified problem

--- task/jwt-task.js
+++ task/jwt-task.js
@@ -5,12 +5,14 @@ const jwt = require("jsonwebtoken");
 const mongoose = require("mongoose");
 const app = express();
 
-mongoose.connect("mongodb+srv://hellojs:hello12345@cluster0.tzhvj.mongodb.net/userDatabase2");
+mongoose.connect(
+  "mongodb+srv://hellojs:hello12345@cluster0.tzhvj.mongodb.net/userDatabase2"
+);
 
 const userSchema = new mongoose.Schema({
-    username: { type: String, required: true },
-    email: { type: String, required: true },
-    password: { type: String, required: true },
+  username: { type: String, required: true },
+  email: { type: String, required: true },
+  password: { type: String, required: true },
 });
 
 const User = mongoose.model("User", userSchema);
@@ -18,31 +20,35 @@ const User = mongoose.model("User", userSchema);
 app.use(express.json());
 
 app.post("/signup", async (req, res) => {
-    const { username, email, password } = req.body;
+  const { username, email, password } = req.body;
 
-    try {
-        let existingUser = false;
-        existingUser = await User.findOne({ email: email });
+  try {
+    let existingUser = false;
+    existingUser = await User.findOne({ email: email });
 
-        if (existingUser) {
-            return res.status(403).json({
-                msg: "User already exits.",
-            });
-        } else {
-            const user = new User({ username: username, email: email, password: password });
-            user.save();
-            res.status(200).json({
-                msg: "User added to database successfully.",
-            });
-        }
-    } catch (error) {
-        res.status(500).json({
-            msg: "An error occurred while adding the user.",
-            error: error.message,
-        });
+    if (existingUser) {
+      return res.status(403).json({
+        msg: "User already exits.",
+      });
+    } else {
+      const user = new User({
+        username: username,
+        email: email,
+        password: password,
+      });
+      user.save();
+      res.status(200).json({
+        msg: "User added to database successfully.",
+      });
     }
+  } catch (error) {
+    res.status(500).json({
+      msg: "An error occurred while adding the user.",
+      error: error.message,
+    });
+  }
 });
 
 app.listen(3000, () => {
-    console.log("Sever is up on port 3000");
+  console.log("Sever is up on port 3000");
 });
