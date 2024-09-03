1ba460000910af6bb100a7a3db22a0f2b91ec921
Rubel <work.mdrubelahamed@gmail.com>
2024-09-03 21:50:09 +0530

    Create the full functionality of password manager
    the password manager use tailwind css, and four state of react -> useCallback, useEffect, useRef, useState, useCallback is for memoization and the useEffect is for when anything change the password manager function should get run and use for selecting the password and copy it to the clipboard and passwordRef is to highlight the password which give a look that the password get copied

--- class-4/password-generator/src/App.jsx
+++ class-4/password-generator/src/App.jsx
@@ -1,4 +1,4 @@
-import { useCallback, useState } from "react";
+import { useCallback, useEffect, useState, useRef } from "react";
 import "./App.css";
 
 function App() {
@@ -7,37 +7,59 @@ function App() {
     const [characterAllowed, setCharacterAllowed] = useState(false);
     const [password, setPassword] = useState("");
 
-    const passwordGenerator = () =>
-        useCallback(() => {
-            let pass = "";
-            let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    // useRef hook
+    const passwordRef = useRef(null);
 
-            if (numberAllowed) {
-                str += "0123456789";
-            }
-            if (characterAllowed) {
-                str += "!#$%&*+?@~";
-            }
+    const passwordGenerator = useCallback(() => {
+        let pass = "";
+        let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 
-            for (let i = 1; i <= length; i++) {
-                let char = Math.floor(Math.random() * str.length + 1);
-                pass = str.charAt(char);
-            }
-            setPassword(pass);
-        }, [length, numberAllowed, characterAllowed, setPassword]);
+        if (numberAllowed) {
+            str += "0123456789";
+        }
+        if (characterAllowed) {
+            str += "!#$%&*+?@~!#$%&*+?@~";
+        }
+
+        // generate random password
+        for (let i = 1; i <= length; i++) {
+            let char = Math.floor(Math.random() * str.length + 1); // take random char from the str
+            pass += str.charAt(char);
+        }
+
+        setPassword(pass);
+    }, [length, numberAllowed, characterAllowed, setPassword]); // these depenndecy setPassword is for memoization(cache)
+
+    // copyPasswordToClipboard function
+    const copyPasswordToClipboard = useCallback(() => {
+        // passwordRef optionmization it will just select text which will give a effect that we select the password
+        passwordRef.current?.select();
+        // passwordRef.current?.setSelectionRange(0, 101);
+        window.navigator.clipboard.writeText(password);
+    }, [password]);
+
+    useEffect(() => {
+        passwordGenerator();
+    }, [length, numberAllowed, characterAllowed, passwordGenerator]); // these dependency if anything change run the func
 
     return (
-        <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 text-orange-500">
-            <h1 className="text-white text-center my-3">Password generator</h1>
-            <div className="flex shadow rounded-lg overflow-hidden mb-4">
+        <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-orange-600  text-black">
+            <h1 className="text-white text-center my-3 font-bold font-mono  text-2xl">
+                Password generator
+            </h1>
+            <div className="flex shadow rounded-lg overflow-hidden mb-4 font-mono">
                 <input
                     type="text"
                     value={password}
-                    className="outline-none w-full py-1 px-3"
+                    className="rounded-2xl outline-none w-full py-2 px-3 m-1"
                     placeholder="Password"
                     readOnly
+                    ref={passwordRef}
                 />
-                <button className="outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0">
+                <button
+                    onClick={copyPasswordToClipboard}
+                    className="rounded-2xl outline-none bg-blue-700 text-white py-2 px-4 m-1 shrink-0"
+                >
                     copy
                 </button>
             </div>
@@ -45,7 +67,7 @@ function App() {
                 <div className="flex items-center gap-x-1">
                     <input
                         type="range"
-                        min={6}
+                        min={8}
                         max={100}
                         value={length}
                         className="cursor-pointer"
@@ -53,7 +75,7 @@ function App() {
                             setLength(e.target.value);
                         }}
                     />
-                    <label>Length: {length}</label>
+                    <label className="italic">Length: {length}</label>
                 </div>
                 <div className="flex items-center gap-x-1">
                     <input
@@ -64,7 +86,9 @@ function App() {
                             setNumberAllowed((prev) => !prev);
                         }}
                     />
-                    <label htmlFor="numberInput">Numbers</label>
+                    <label className="italic" htmlFor="numberInput">
+                        Numbers
+                    </label>
                 </div>
                 <div className="flex items-center gap-x-1">
                     <input
@@ -75,7 +99,9 @@ function App() {
                             setCharacterAllowed((prev) => !prev);
                         }}
                     />
-                    <label htmlFor="characterInput">Characters</label>
+                    <label className="italic" htmlFor="characterInput">
+                        Characters
+                    </label>
                 </div>
             </div>
         </div>
